efd526714858be1064ff74fe39f9b1a847fb7606
Jorge Guevara <jorgeaguevaram@gmail.com>
2024-02-03 10:02:17 -0300

    projeto terminado

---
+++ script.js
@@ -0,0 +1,17 @@
+const form = document.querySelector('.formularioFaleConosco')
+const mask = document.querySelector('.capaFormulario')
+const btn = document.querySelector('.entreEmContato')
+
+function addMask (){
+    form.style.left = '50%'
+    form.style.transform = 'translateX(-50%)'
+    mask.style.visibility = 'visible'
+}
+
+
+btn.addEventListener('click', addMask)
+mask.addEventListener('click', () => {
+    form.style.left = '-280px'
+    form.style.transform = 'translateX(0%)'
+    mask.style.visibility = 'hidden'
+})
